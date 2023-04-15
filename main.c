e6efb60781d62518ba2b9b0d835e0290fa71450e
Abril santisteban <abril.santisteban@gmail.com>
2023-04-15 12:27:48 -0300

    imprimir y blink con timers virtuales

--- main.c
+++ main.c
@@ -7,26 +7,42 @@
 #include <string.h>
 
 static virtual_timer_t led_vt;
+static virtual_timer_t serial_vt;
 
 
 //LED timer callback
 static void led_cb(void *arg) {
 
   palToggleLine(LINE_LED_GREEN); //LED_toggle
-  chVTSet(&led_vt, TIME_MS2I(100), led_cb, NULL);
+  chSysLockFromISR();
+  chVTSetI(&led_vt, TIME_MS2I(500), led_cb, NULL);
+  chSysUnlockFromISR();
   
 }
+
+//Serial Port timer2 callback
+static void serial_cb(void *arg) {
+
+    sdWrite(&LPSD1,"Hello world\r\n", strlen("Hello world\r\n"));
+    chSysLockFromISR();
+    chVTSetI(&serial_vt, TIME_MS2I(1000), serial_cb, NULL);
+    chSysUnlockFromISR();
+  
+}
+
 
 int main(void) {
 
   halInit();
   chSysInit();
 
+  sdStart(&LPSD1, NULL);
+
   /* LED timer initialization.*/
   chVTObjectInit(&led_vt);
+  chVTObjectInit(&serial_vt);
 
-
-  chVTSet(&led_vt, TIME_MS2I(100), led_cb, NULL);
+  chVTSet(&led_vt, TIME_MS2I(1000), led_cb, NULL);
 
   return 0;
 }
