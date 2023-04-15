896939dc11876cc6b6875a88bd0414569b519587
Abril santisteban <abril.santisteban@gmail.com>
2023-04-15 12:55:56 -0300

    thread imprimir y blink con timer virtual

--- main.c
+++ main.c
@@ -13,9 +13,30 @@ static virtual_timer_t led_vt;
 static void led_cb(void *arg) {
 
   palToggleLine(LINE_LED_GREEN); //LED_toggle
-  chVTSet(&led_vt, TIME_MS2I(100), led_cb, NULL);
+  chSysLockFromISR();
+  chVTSetI(&led_vt, TIME_MS2I(500), led_cb, NULL);
+  chSysUnlockFromISR();
   
 }
+
+static THD_WORKING_AREA(waThread2, 1024);
+static THD_FUNCTION(Thread2, arg) {
+
+  (void)arg;
+  chRegSetThreadName("PuertoSerie");
+  /*
+   * Activates the serial driver 2 using the driver default configuration.
+   */
+
+  sdStart(&LPSD1, NULL);
+
+  while(true) {
+    sdWrite(&LPSD1,"Hello world\r\n", strlen("Hello world\r\n"));
+    chThdSleepMilliseconds(500);
+  }
+}
+
+
 
 int main(void) {
 
@@ -24,9 +45,10 @@ int main(void) {
 
   /* LED timer initialization.*/
   chVTObjectInit(&led_vt);
-
+  chThdCreateStatic(waThread2, sizeof(waThread2), NORMALPRIO+1, Thread2, NULL);
 
   chVTSet(&led_vt, TIME_MS2I(100), led_cb, NULL);
+
 
   return 0;
 }
