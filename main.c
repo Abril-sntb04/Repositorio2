853fd6b6113bb173d43e71af7b6d796364fcdd2c
Abril santisteban <abril.santisteban@gmail.com>
2023-04-15 11:52:36 -0300

    blink led con timer virtual

--- main.c
+++ main.c
@@ -6,64 +6,27 @@
 #include "oslib_test_root.h"
 #include <string.h>
 
-/*
- * Green LED blinker thread, times are in milliseconds.
- */
-static THD_WORKING_AREA(waThread1, 1024);
-static THD_FUNCTION(Thread1, arg) {
+static virtual_timer_t led_vt;
 
-  (void)arg;
-  chRegSetThreadName("blinker");
-  while (true) {
-    palToggleLine(LINE_LED_GREEN);
-    chThdSleepMilliseconds(100);
-  }
+
+//LED timer callback
+static void led_cb(void *arg) {
+
+  palToggleLine(LINE_LED_GREEN); //LED_toggle
+  chVTSet(&led_vt, TIME_MS2I(100), led_cb, NULL);
+  
 }
 
-
-static THD_WORKING_AREA(waThread2, 1024);
-static THD_FUNCTION(Thread2, arg) {
-
-  (void)arg;
-  chRegSetThreadName("PuertoSerie");
-
-  sdStart(&LPSD1, NULL);
-
-  while(true) {
-    sdWrite(&LPSD1,"Hello world\r\n", strlen("Hello world\r\n"));
-    chThdSleepMilliseconds(500);
-  }
-}
-
-
-/*
- * Application entry point.
- */
 int main(void) {
 
-  /*
-   * System initializations.
-   * - HAL initialization, this also initializes the configured device drivers
-   *   and performs the board-specific initializations.
-   * - Kernel initialization, the main() function becomes a thread and the
-   *   RTOS is active.
-   */
   halInit();
   chSysInit();
 
+  /* LED timer initialization.*/
+  chVTObjectInit(&led_vt);
 
-  /*
-   * Creates the blinker thread.
-   */
-  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
-  chThdCreateStatic(waThread2, sizeof(waThread2), NORMALPRIO+1, Thread2, NULL);
-  /*
-   * Normal main() thread activity, in this demo it does nothing except
-   * sleeping in a loop and check the button state.
-   */
-  while(true)
-  {
-    chThdSleep(100);
-  }
+
+  chVTSet(&led_vt, TIME_MS2I(100), led_cb, NULL);
+
   return 0;
 }
