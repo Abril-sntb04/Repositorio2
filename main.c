d112fe92ecc3f06ded410f9edfcd1c5642ab8baf
Abril santisteban <abril.santisteban@gmail.com>
2023-04-16 09:21:51 -0300

    Timers y thread aparte (no funciona) 3

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "oslib_test_root.h"
 #include <string.h>
 
-//static virtual_timer_t led_vt;
+static virtual_timer_t led_vt;
 static virtual_timer_t serial_vt;
 
 static thread_reference_t trp = NULL;
@@ -14,7 +14,7 @@ static thread_reference_t trp = NULL;
 //LED timer callback
 static void led_cb(void *arg) {
 
-  palToggleLine(LINE_LED_GREEN); //LED_toggle
+  
   chSysLockFromISR();
   chVTSetI(&led_vt, TIME_MS2I(100), led_cb, NULL);
   chSysUnlockFromISR();
@@ -24,18 +24,26 @@ static void led_cb(void *arg) {
 //Serial Port timer2 callback
 static void serial_cb(void *arg) {
 
+    
     chSysLockFromISR();
     chThdResumeI(&trp, (msg_t)0x1337); 
+
+    //Seteamos el timer de vuelta para que siga imprimiendo cada 100ms
+    chVTSetI(&serial_vt, TIME_MS2I(1000), serial_cb, NULL);
+
     chSysUnlockFromISR();
   
 }
 
-static THD_WORKING_AREA(waThread, 1024);
-static THD_FUNCTION(Thread, arg) {
+static THD_WORKING_AREA(serial_thd_wa, 1024);
+static THD_FUNCTION(serial_thd, arg) {
 
   (void)arg;
   chRegSetThreadName("ImprimirPuerto");
+  palToggleLine(LINE_LED_GREEN); //LED_toggle
   sdStart(&LPSD1, NULL);
+  sdWrite(&LPSD1,"Hello world\r\n", strlen("Hello world\r\n"));
+  chVTSet(&serial_vt, TIME_MS2I(1000), serial_cb, NULL);
 
   while(true) {
   msg_t msg;
@@ -48,11 +56,8 @@ static THD_FUNCTION(Thread, arg) {
     chSysUnlock();
 
     //Desde el callback del timer se reanuda y sigue con sus operaciones
-
+    
     sdWrite(&LPSD1,"Hello world\r\n", strlen("Hello world\r\n"));
-
-    //Seteamos el timer de vuelta para que siga imprimiendo cada 100ms
-    chVTSet(&serial_vt, TIME_MS2I(1000), serial_cb, NULL);
 
   }
 }
@@ -66,11 +71,9 @@ int main(void) {
   chVTObjectInit(&led_vt);
   chVTObjectInit(&serial_vt);
 
-  chThdCreateStatic(waThread, sizeof(waThread), NORMALPRIO, Thread, NULL);
+  chThdCreateStatic(serial_thd, sizeof(serial_thd_wa), NORMALPRIO+3, serial_thd, NULL);
 
-  chVTSet(&serial_vt, TIME_MS2I(1000), serial_cb, NULL);
   chVTSet(&led_vt, TIME_MS2I(100), led_cb, NULL);
-
   
   return 0;
 }
